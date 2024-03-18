dbe58ffaeb33ba8a18bcc7b884990bb48a7134b7
Abdullah Akhtar <44116978+aaydee99@users.noreply.github.com>
2024-03-18 06:36:55 +0500

    Final Commit to frontend test

--- frontend_challenge/client/src/App.js
+++ frontend_challenge/client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import SearchBar from './SearchBar';
+import { SearchBar } from './components';
 
 function App() {
   return (
     <div className="App">
-      <SearchBar/>
+      <SearchBar />
     </div>
   );
 }
